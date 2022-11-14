8d1173199cb19b69e69cbf91eb9cab5b1fc02bd0
Papallism <papallis.m@unic.ac.cy>
2022-11-14 15:30:47 +0200

    Added add button functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,8 @@ const modalElement = document.getElementById("add-modal")
 const startAddMovieButton = document.querySelector("header button")
 const backdrop = document.getElementById("backdrop")
 const modalCancelButton = modalElement.querySelector(".btn--passive")
+const modalAddMovieButton = modalElement.querySelector(".btn--success")
+const userInputElements = modalElement.querySelectorAll("input")
 
 function toggleBackdrop() {
   backdrop.classList.toggle('visible')
@@ -16,10 +18,22 @@ function backdropClickHandler() {
   toggleMovieModal()
 }
 
-function cancelAddMovie(params) {
+function cancelAddMovie() {
   toggleMovieModal()
+}
+
+function addMovieHandler() {
+  const titleInput = userInputElements[0].value.trim()
+  const imageUrlInput = userInputElements[1].value.trim()
+  const ratingInput = userInputElements[2].value.trim()
+
+  if (titleInput === '' || imageUrlInput === '' || ratingInput === ''
+  || +ratingInput < 1 || +ratingInput > 5) {
+    alert("Enter valid values.")
+  }
 }
 
 startAddMovieButton.addEventListener('click', toggleMovieModal)
 backdrop.addEventListener('click', backdropClickHandler)
 modalCancelButton.addEventListener('click', cancelAddMovie)
+modalAddMovieButton.addEventListener('click', addMovieHandler)
