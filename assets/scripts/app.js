898e6bebd57c5c70f28ed3c4af63094c6b59eb0d
Papallism <papallis.m@unic.ac.cy>
2022-11-14 15:50:25 +0200

    Added movie list render

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,7 @@ const backdrop = document.getElementById("backdrop")
 const modalCancelButton = modalElement.querySelector(".btn--passive")
 const modalAddMovieButton = modalElement.querySelector(".btn--success")
 const userInputElements = modalElement.querySelectorAll("input")
+const movieEntriesElement = document.getElementById("entry-text")
 
 const movies = []
 
@@ -45,12 +46,39 @@ function addMovieHandler() {
   movies.push(newMovie)
   toggleMovieModal()
   clearUserInputs()
+  updateUI()
+  renderNewMovieElement(titleInput, imageUrlInput, ratingInput)
 }
 
 function clearUserInputs() {
   for (const input of userInputElements) {
     input.value = ''
   }
+}
+
+function updateUI() {
+  if (movies.length === 0) {
+    movieEntriesElement.style.display = 'none'
+  } else {
+    movieEntriesElement.style.display = 'block'
+  }
+}
+
+function renderNewMovieElement(title, imageUrl, rating) {
+  const newMovieElement = document.createElement('li')
+  newMovieElement.className = 'movie-element'
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+      <img src="${imageUrl}">
+    </div>
+    <div class="movie-element__info">
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+    </div>
+  `
+
+  const movieList = document.getElementById("movie-list")
+  movieList.append(newMovieElement)
 }
 
 startAddMovieButton.addEventListener('click', toggleMovieModal)
